62a6adc014a568c842b494e860e96e441dca8b2e
Muddasir khuwaja <engr.muddasir01@gmail.com>
2026-05-14 12:22:37 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { tanstackPlugin } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    tanstackPlugin(),
+    react(),
+    tsconfigPaths()
+  ],
   preview: {
     allowedHosts: ['maxoto-q8qb.onrender.com']
   }
