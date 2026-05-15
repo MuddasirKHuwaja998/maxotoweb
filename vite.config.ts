f6eb754c4af56993f3bc440ae1d0bcc871bc6be5
Muddasir khuwaja <engr.muddasir01@gmail.com>
2026-05-15 10:46:22 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from '@lovable.dev/vite-tanstack-config'
 
 export default defineConfig({
-  tanstackStart: {
-    server: { entry: 'server' },
-  },
   vite: {
     preview: {
       allowedHosts: ['maxoto-q8qb.onrender.com'],
